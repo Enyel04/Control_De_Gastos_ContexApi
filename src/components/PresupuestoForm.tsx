b14c2445bd683e9f8e0a7ab8104f0863b4916460
Angel Mendez <110472083+Enyel04@users.noreply.github.com>
2024-06-18 15:30:50 -0400

    fixed del tailwind, tambien agregando estilos css

--- src/components/PresupuestoForm.tsx
+++ src/components/PresupuestoForm.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+
 
 export default function Form() {
   return (
-    <div>
-        <p>Formulario aqui</p>
-    </div>
+    
+    <form className='space-y-5'>
+      <div className='flex flex-col space-y-5'>
+          <label htmlFor="presupuesto" className=' text-4xl text-sky-600 font-bold  text-center'>
+              Definir Presupuesto 
+          </label>
+
+          <input
+                id='presupuesto' 
+                type="number" 
+                className=" w-full bg-white border bordger-gray-200 p2"
+                placeholder='Definir tu presupuesto '
+                name='presupuesto'
+          />
+      </div>
+
+      <input 
+          type="submit" 
+          value='Definir Presupuesto'
+          className=' bg-sky-600 hover:bg-sky-700 cursor-pointer w-full p-2 text-white font-black uppercase'
+      
+      />
+
+
+    </form>
   )
 }
